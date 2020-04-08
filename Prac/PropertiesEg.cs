38bb24710b21cc49a292ad02b64bee0125cb3f95
Subhaswini <cjsubbu@gmail.com>
2020-04-08 17:12:21 +0530

    PropertiesExample

---
+++ Prac/PropertiesEg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prac
+{
+    class Person
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    class PropertiesEg
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
