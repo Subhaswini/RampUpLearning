f0e8cc6c2050b06175c6025c5a1c8dcf95ebef38
Subhaswini <cjsubbu@gmail.com>
2020-04-09 18:25:02 +0530

    Method Overloading and Method Overriding

---
+++ Prac/MethodOverloading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prac
+{
+    public class MethodOverloading
+    {
+        public static void Main()
+        {
+            MethodOverloading M = new MethodOverloading();
+            M.Add(5, 4);
+            M.Add(5.0, 4.0);
+            M.Add(5, 5.0);
+            M.Add(5, 5, 5);
+        }
+
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Sum={0}", a + b);
+        }
+        public void Add(double a, double b)
+        {
+            Console.WriteLine("Sum1={0}", a + b);
+        }
+
+        public void Add(int a, double b)
+        {
+            Console.WriteLine("Sum2={0}", a + b);
+        }
+
+        public void Add(int a, int b,int c)
+        {
+            Console.WriteLine("Sum3={0}", a + b + c);
+        }
+
+    }
+}
---
+++ Prac/Polymorphism(MO).cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prac
+{
+    public class Employee1
+    {
+        public string FirstName = "CJ";
+        public string LastName = "Subhaswini";
+
+        public virtual void PrintFullName1()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+
+    public class FullTimeEmployee1 : Employee1
+    {
+        public override void PrintFullName1()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " - Full Time");
+        }
+    }
+
+    public class PartTimeEmployee1 : Employee1
+    {
+        public override void PrintFullName1()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " - Part Time");
+        }
+    }
+    public class Polymorphism_MO_
+    {
+        public static void Main()
+        {
+            Employee1[] employees = new Employee1[3];
+            employees[0] = new Employee1();
+            employees[1] = new FullTimeEmployee1();
+            employees[2] = new PartTimeEmployee1();
+
+            foreach (Employee1 e in employees)
+            {
+                e.PrintFullName1();
+            }
+
+        }
+    }
+}
