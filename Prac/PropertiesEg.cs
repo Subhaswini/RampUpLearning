29c61f579d7d42606008b8ffd452070aea7026b4
Subhaswini <cjsubbu@gmail.com>
2020-04-09 19:08:20 +0530

    Properites Example Modification

--- Prac/PropertiesEg.cs
+++ Prac/PropertiesEg.cs
@@ -6,20 +6,62 @@ namespace Prac
 {
     class Person
     {
-        private string name; // field
-        public string Name   // property
+        private int id;
+        private string fname;
+        private int group = 1;
+        
+        public int Id
         {
-            get { return name; }
-            set { name = value; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Student Id cannot be negative");
+                }
+                this.id = value;
+            }
+            get
+            {
+                return this.id;
+            }
         }
+        public string FName
+        {
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name cannot be null or empty");
+                }
+                this.fname = value;
+            }
+            get
+            {
+                return string.IsNullOrEmpty(this.fname)?"No Name": this.fname;
+            }
+           
+        }
+        public int Group
+        {
+            get
+            {
+                return this.group;
+            }
+        }
+     
+
     }
     class PropertiesEg
     {
         static void Main(string[] args)
         {
-            Person myObj = new Person();
-            myObj.Name = "Liam";
-            Console.WriteLine(myObj.Name);
+            Person P = new Person();
+            P.Id = 100;
+            P.FName = "CJ";
+
+            Console.WriteLine("Student id={0}",P.Id);
+            Console.WriteLine("Student Fname={0}", P.FName);
+            Console.WriteLine("Student group={0}", P.Group);
         }
     }
 }
