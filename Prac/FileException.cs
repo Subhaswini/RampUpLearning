83e4c8db5e911e34456d71772434024dc0364743
Subhaswini <cjsubbu@gmail.com>
2020-04-10 13:26:57 +0530

    CSharpExceptions

---
+++ Prac/FileException.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Prac
+{
+    class FileException
+    {
+        public static void Main()
+        {
+            StreamReader streamReader = null;
+            
+            try
+            {
+                streamReader = new StreamReader(@"C:\SampleFile\data.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+
+            //FileNotFoundException
+            catch (FileNotFoundException ex)
+            {
+                /*Console.WriteLine(ex.Message);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(ex.StackTrace);*/
+                Console.WriteLine("Please check if the file {0} exists", ex.FileName);
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if(streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                
+                Console.WriteLine("Finally block!");
+            }
+          
+        }
+    }
+}
---
+++ Prac/InnerException.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Prac
+{
+    class InnerException
+    {
+        public static void Main()
+        {
+            //NullReferenceException
+            try
+            {
+                //fileNotFoundException
+                try
+                {
+                    //DivideByZeroException
+                    try
+                    {
+
+                        Console.WriteLine("Enter First Number:");
+                        int FNo = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Enter Second Number:");
+                        int SNo = Convert.ToInt32(Console.ReadLine());
+
+                        int Result = FNo / SNo;
+
+                        Console.WriteLine("Result={0}", Result);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        string filePath = @"C:\SampleFile\log.txt";
+                        if (File.Exists(filePath))
+                        {
+                            StreamWriter sw = new StreamWriter(filePath);
+                            sw.Write(ex.GetType().Name);
+                            sw.WriteLine();
+                            sw.Write(ex.Message);
+                            sw.Close();
+                            Console.WriteLine("Please try again later..");
+                        }
+                        else
+                        {
+                            throw new FileNotFoundException(filePath + "is not present");
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Outer exception:{0}", exception.GetType().Name);
+                    if (exception.InnerException.GetType().Name != null)
+                    {
+                        Console.WriteLine("Inner exception:{0}", exception.InnerException.GetType().Name);
+                    }
+                    else
+                    {
+                        throw new NullReferenceException();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("FilePath is Empty.");
+                Console.Write("CurrentException:{0}", e.GetType().Name);
+            }
+        }
+    }
+}
