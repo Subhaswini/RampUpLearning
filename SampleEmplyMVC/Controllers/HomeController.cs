649ca02a23e09bded003003715f192e0d440b524
Subhaswini <cjsubbu@gmail.com>
2020-04-29 01:06:30 +0530

    First Commit Of ASP.NET CORE MVC

---
+++ SampleEmplyMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleEmplyMVC.Models;
+
+namespace SampleEmplyMVC.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            Movie movie = new Movie()
+            {
+                Title ="The Godfather",
+                ReleaseDate = new DateTime(1972,3,24) 
+            };
+            return View(movie);
+        }
+
+        public IActionResult LayoutTest()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
---
+++ SampleEmplyMVC/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleEmplyMVC.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SampleEmplyMVC.Controllers
+{
+    public class UserController: Controller
+    {
+        public IActionResult Index()
+        {
+            //User user = new User()
+            //{
+            //    FirstName = "CJ",
+            //    LastName = "Subhaswini",
+            //    Birthday = new DateTime(1998, 3, 25)
+            //};
+            List<User> user = new List<User>()
+            {
+                new User() { LastName= "Subhaswini", FirstName = "CJ", Birthday = new DateTime(1998,03,25) },
+                new User() { LastName="Sharmila", FirstName ="A", Birthday = new DateTime(1998,05,22) },
+                new User() { LastName="Sneha", FirstName = " Prasanna", Birthday = new DateTime(1998,07,21) }
+            };
+            return View(user);
+        }
+    }
+}
